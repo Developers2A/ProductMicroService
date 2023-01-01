414038db69ec8ef3bc90ebd40ea6cb2780dc5d1a
bahraman <bahraman@gmail.com>
2023-01-01 13:41:07 +0330

    init ContractService

---
+++ src/Contract/Postex.Contract.Domain/Enum/ContractType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postex.Contract.Domain.Enums
+{
+    public enum ContractType
+    {
+        [Description("قرارداد پیش فرض")]
+        Default = 1,
+        [Description("قرارداد مشتری")]
+        Customer = 2,
+        [Description("قرارداد نماینده")]
+        Agent = 3,
+        [Description("قرارداد شهر")]
+        City = 4
+    }
+}
