ecdc947ee021177ea049b8fca98b9e17a92a791c
bahraman <bahraman@gmail.com>
2023-01-16 14:09:11 +0330

    Add ContractAccountingTemplate

---
+++ src/Contract/Postex.Contract.Application/Features/ContractAccountingTemplates/Commands/Create/CreateContractAccountingTemplateCommand.cs
@@ -0,0 +1,9 @@
+﻿using Postex.Contract.Application.Contracts;
+using Postex.Contract.Domain;
+
+namespace Postex.Contract.Application.Features.ContractAccountingTemplates.Commands.Create
+{
+    public class CreateContractAccountingTemplateCommand: ContractAccountingTemplate,ITransactionRequest
+    {
+    }
+}
---
+++ src/Contract/Postex.Contract.Domain/ContractAccountingTemplate.cs
@@ -0,0 +1,19 @@
+﻿using Postex.SharedKernel.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postex.Contract.Domain
+{
+    public class ContractAccountingTemplate : BaseEntity<int>
+    {              
+        public string ContractDetailType { get; set; }
+        public int ContractDetailId { get; set; }
+        public int AccountId { get; set; }
+        public double PercentValue { get; set; }
+        public int FixedValue { get; set; }
+        public string Description { get; set; }
+    }
+}
