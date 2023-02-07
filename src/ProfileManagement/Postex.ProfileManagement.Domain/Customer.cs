0bb7358c48f05f053de3537c74b02a09451b3b2d
bahraman <bahraman@gmail.com>
2023-02-07 23:16:30 +0330

    Add Profile Entities

---
+++ src/ProfileManagement/Postex.ProfileManagement.Domain/Customer.cs
@@ -0,0 +1,16 @@
+﻿using Postex.SharedKernel.Domain;
+
+namespace Postex.ProfileManagement.Domain
+{
+    public class Customer : BaseEntity<int>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FatherName { get; set; }
+        public string NationalCode { get; set; }
+        public string Email { get; set; }
+        public int MobileNo { get; set; }
+        public string PostalCode { get; set; }
+        public bool isShahkarValidate  { get; set; }
+    }
+}
