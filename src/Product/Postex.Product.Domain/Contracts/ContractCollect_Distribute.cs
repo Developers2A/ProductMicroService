5d27504fdd3994921448fe5749d57a239fecee16
bahraman <bahraman@gmail.com>
2023-02-19 16:49:01 +0330

    Add Contract.Domain to Product.Domain

---
+++ src/Product/Postex.Product.Domain/Contracts/ContractCollect_Distribute.cs
@@ -0,0 +1,18 @@
+﻿using Postex.SharedKernel.Domain;
+
+namespace Postex.Product.Domain;
+
+public class ContractCollect_Distribute : BaseEntity<int>
+{
+    public ContractInfo ContractInfo { get; set; }
+    public int ContractInfoId { get; set; }
+    public int? ProvinceId { get; set; }
+    public int? CityId { get; set; }
+    public int BoxTypeId { get; set; }
+    public BoxType BoxType { get; set; }
+    public double SalePrice { get; set; }
+    public double BuyPrice { get; set; }
+    public string Description { get; set; }
+    public bool IsActice { get; set; }
+
+}
---
+++ src/Product/Postex.Product.Domain/Contracts/ContractCourier.cs
@@ -0,0 +1,21 @@
+﻿using Postex.SharedKernel.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postex.Product.Domain
+{
+    public class ContractCourier : BaseEntity<int>
+    {
+        public ContractInfo ContractInfo { get; set; }
+        public int ContractInfoId { get; set; }
+        public int CourierId { get; set; }
+        public int FixedDiscount { get; set; }
+        public double PercentDiscount { get; set; }
+        public bool IsActive { get; set; }
+        public string Description { get; set; }
+
+    }
+}
