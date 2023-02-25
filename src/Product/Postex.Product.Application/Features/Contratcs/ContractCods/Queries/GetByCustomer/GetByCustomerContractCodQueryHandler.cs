566255e751e2b09f88d1094db404cf8d95fa1fe2
bahraman <bahraman@gmail.com>
2023-02-25 15:17:10 +0330

    Check Date for Active contract in ContractCod

--- src/Product/Postex.Product.Application/Features/Contratcs/ContractCods/Queries/GetByCustomer/GetByCustomerContractCodQueryHandler.cs
+++ src/Product/Postex.Product.Application/Features/Contratcs/ContractCods/Queries/GetByCustomer/GetByCustomerContractCodQueryHandler.cs
@@ -22,7 +22,8 @@ namespace Postex.Product.Application.Features.Contratcs.ContractCods.Queries.Get
         public async Task<List<ContractCodDto>> Handle(GetByCustomerContractCodQuery request, CancellationToken cancellationToken)
         {
             var codCus = await _readRepository.Table.AsNoTracking()
-               .Include(c => c.ContractInfo).Where(c => c.ContractInfo.IsActive == true && c.ContractInfo.CustomerId == request.CustomerId)
+               .Include(c => c.ContractInfo)
+               .Where(c => c.ContractInfo.IsActive == true && c.ContractInfo.CustomerId == request.CustomerId && c.ContractInfo.StartDate <= DateTime.Now && c.ContractInfo.EndDate >= DateTime.Now)
                .Select(c => new ContractCodDto
                {
                    Id = c.Id,
@@ -39,28 +40,47 @@ namespace Postex.Product.Application.Features.Contratcs.ContractCods.Queries.Get
             if (codCus.Count != 0)
                 return codCus;
 
-
-
             var codCity = await _readRepository.Table
-              .Include(c => c.ContractInfo).Where(c => c.ContractInfo.IsActive == true && c.ContractInfo.CityId == request.CityId && c.ContractInfo.CustomerId == null)
-              .Select(c => new ContractCodDto
-              {
-                  Id = c.Id,
-                  ContractInfoId = c.ContractInfoId,
-                  FromValue = c.FromValue,
-                  ToValue = c.ToValue,
-                  FixedPercent = c.FixedPercent,
-                  FixedValue = c.FixedValue,
-                  Description = c.Description,
-                  IsActice = c.IsActice,
-                  LevelPrice = "City"
-              })
-              .ToListAsync(cancellationToken);
+                 .Include(c => c.ContractInfo)
+                 .Where(c => c.ContractInfo.IsActive == true && c.ContractInfo.CityId == request.CityId  && c.ContractInfo.CustomerId == 0 && c.ContractInfo.StartDate <= DateTime.Now && c.ContractInfo.EndDate >= DateTime.Now)
+                 .Select(c => new ContractCodDto
+                 {
+                     Id = c.Id,
+                     ContractInfoId = c.ContractInfoId,
+                     FromValue = c.FromValue,
+                     ToValue = c.ToValue,
+                     FixedPercent = c.FixedPercent,
+                     FixedValue = c.FixedValue,
+                     Description = c.Description,
+                     IsActice = c.IsActice,
+                     LevelPrice = "City"
+                 })
+                 .ToListAsync(cancellationToken);
             if (codCity.Count != 0)
                 return codCity;
 
+            var codProvince = await _readRepository.Table
+                .Include(c => c.ContractInfo)
+                .Where(c => c.ContractInfo.IsActive == true && c.ContractInfo.ProvinceId == request.ProvinceId && c.ContractInfo.CityId == 0 && c.ContractInfo.CustomerId == 0 && c.ContractInfo.StartDate <= DateTime.Now && c.ContractInfo.EndDate >= DateTime.Now)
+                .Select(c => new ContractCodDto
+                {
+                    Id = c.Id,
+                    ContractInfoId = c.ContractInfoId,
+                    FromValue = c.FromValue,
+                    ToValue = c.ToValue,
+                    FixedPercent = c.FixedPercent,
+                    FixedValue = c.FixedValue,
+                    Description = c.Description,
+                    IsActice = c.IsActice,
+                    LevelPrice = "Province"
+                })
+                .ToListAsync(cancellationToken);
+            if (codProvince.Count != 0)
+                return codProvince;
+
             var codDefualt = await _readRepository.Table
-           .Include(c => c.ContractInfo).Where(c => c.ContractInfo.IsActive == true && c.ContractInfo.CustomerId == null && c.ContractInfo.CityId == null && c.ContractInfo.ProvinceId == null)
+           .Include(c => c.ContractInfo)
+           .Where(c => c.ContractInfo.IsActive == true && c.ContractInfo.CustomerId == 0 && c.ContractInfo.CityId == 0 && c.ContractInfo.ProvinceId == 0 && c.ContractInfo.StartDate <= DateTime.Now && c.ContractInfo.EndDate >= DateTime.Now)
            .Select(c => new ContractCodDto
            {
                Id = c.Id,
