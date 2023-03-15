01f9cfb62d62bd84125d5b5ff3b6e72ad90d0c80
bahraman <bahraman@gmail.com>
2023-03-15 17:19:42 +0330

    change customerId to int inCustomerDto

--- src/UserManagement/Postex.UserManagement.Application/Dtos/Customers/CustomerDto.cs
+++ src/UserManagement/Postex.UserManagement.Application/Dtos/Customers/CustomerDto.cs
@@ -4,7 +4,7 @@ namespace Postex.UserManagement.Application.Dtos.Customers
 {
     public class CustomerDto
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string FatherName { get; set; }
